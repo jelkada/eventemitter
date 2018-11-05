b2b37020f2dbf9a9983980b3ae8f364dbec32a57
Jimmy Elkada <jelkada@yahoo.com>
2018-11-04 20:20:51 -0500

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  accounts = [
+    {
+      name: 'John Smith',
+      status: 'active'
+    },
+    {
+      name: 'Sarah Luis',
+      status: 'inactive'
+    }
+  ];
+
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
+}
